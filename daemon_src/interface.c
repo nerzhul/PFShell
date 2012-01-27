197fefa457d0e32bd982ab26e5c26ffc6df01eb1
Loïc BLOT <loic.blot@frostsapphirestudios.com>
2012-01-27 08:56:36 +0100

    [0088] Move interface.c interface.h to daemon src

---
+++ daemon_src/interface.c
@@ -0,0 +1,403 @@
+/*
+* Copyright (c) 2011-2012, Frost Sapphire Studios
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Frost Sapphire Studios nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "interface.h"
+#include "configuration.h"
+#include "command.h"
+#include "iputils.h"
+
+void addInterface(char* name)
+{
+	net_iface* newIface = (net_iface*)malloc(sizeof(net_iface));
+	newIface->name = (char*)malloc(strlen(name)*sizeof(char));
+	strcpy(newIface->name,name);
+	newIface->next = NULL;
+	newIface->prev = NULL;
+	newIface->ip = "";
+	newIface->acl_in = "";
+	newIface->acl_out = "";
+	newIface->desc = "";
+	newIface->state = 1;
+
+	if(interfaces == NULL)
+	{
+		interfaces = newIface;
+	}
+	else
+	{
+		net_iface* cursor = interfaces;
+
+		while(cursor->next != NULL)
+			cursor = cursor->next;
+
+		newIface->prev = cursor;
+		cursor->next = newIface;
+	}
+}
+
+void loadInterfaces()
+{
+	char input[1024];
+	char output[1024] = "";
+#ifdef FREEBSD
+	execSystemCommand("for IF in $(/sbin/ifconfig | grep HWaddr | awk '{print $1}'); do echo $IF; done;",output);
+#else
+	execSystemCommand("for IF in $(/sbin/ifconfig | grep BROADCAST | awk '{print $1}' | awk -F':' '{print $1}'); do echo $IF; done;",output);
+#endif
+	char* iface[2];
+	cutFirstWord(output,iface);
+	if(strcmp(iface[0],"") != 0)
+	{
+		addInterface(iface[0]);
+		if(strcmp(iface[1],"") != 0)
+		{
+			char* iface2[2];
+			char ifbuffer[1024];
+			strcpy(ifbuffer,iface[1]);
+
+			while(strcmp(ifbuffer,"") != 0)
+			{
+				cutFirstWord(ifbuffer,iface2);
+				net_iface* cursor = interfaces;
+				if(cursor == NULL)
+					addInterface(iface2[0]);
+				else
+				{
+					unsigned short found = 0;
+					while(cursor != NULL && found == 0)
+					{
+						if(strcmp(cursor->name,iface2[0]) != 0)
+							cursor = cursor->next;
+						else
+							found = 1;
+					}
+
+					if(found == 0)
+						addInterface(iface2[0]);
+				}
+				strcpy(ifbuffer,iface2[1]);
+			}
+		}
+	}
+}
+
+unsigned short saveInterfaces()
+{
+	net_iface* cursor = interfaces;
+	if(interfaces == NULL)
+		return 0;
+
+	while(cursor != NULL)
+	{
+		if(strcmp(cursor->ip,"DHCP") == 0)
+		{
+			if(getInterfaceState(cursor->name) == 1)
+			{
+				char buffer[1024];
+				char pathbuffer[100];
+				strcpy(pathbuffer,"/etc/hostname.");
+				strcat(pathbuffer,cursor->name);
+
+				FILE* fIface = fopen(pathbuffer,"w+");
+				fwrite("dhcp\n",1,sizeof("dhcp\n"),fIface);
+				fclose(fIface);
+			}
+		}
+		else
+		{
+			char* ipmask[2];
+			cutFirstWord(cursor->ip,ipmask);
+			if(strlen(ipmask[1]) > 1)
+			{
+				if(regexp(ipmask[0],"^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$") == 0)
+				{
+					if(is_valid_mask(ipmask[1]) == 0)
+					{
+						char buffer[1024];
+						char pathbuffer[100];
+						strcpy(pathbuffer,"/etc/hostname.");
+						strcat(pathbuffer,cursor->name);
+
+						strcpy(buffer,"inet ");
+						strcat(buffer,ipmask[0]);
+						strcat(buffer," ");
+						strcat(buffer,ipmask[1]);
+						strcat(buffer," NONE\n");
+
+						FILE* fIface = fopen(pathbuffer,"w+");
+						fwrite(buffer,1,strlen(buffer),fIface);
+						fclose(fIface);
+					}
+				}
+			}
+			else if(regexp(ipmask[0],"^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])((/([0-9]|[1-2][0-9]|3[0-2]))?)$") == 0)
+			{
+				char* mask[2];
+				cutByChar(ipmask[0],mask,'/');
+
+				char buffer[1024];
+				char pathbuffer[100];
+
+				strcpy(pathbuffer,"/etc/hostname.");
+				strcat(pathbuffer,cursor->name);
+
+				strcpy(buffer,"inet ");
+				strcat(buffer,mask[0]);
+				strcat(buffer," ");
+				strcat(buffer,calc_mask_from_cidr(mask[1]));
+				strcat(buffer," NONE\n");
+
+				FILE* fIface = fopen(pathbuffer,"w+");
+				fwrite(buffer,1,strlen(buffer),fIface);
+				fclose(fIface);
+			}
+		}
+		cursor = cursor->next;
+	}
+
+	return 0;
+}
+
+unsigned short setInterfaceIP(char* name, char* ip)
+{
+	if(interfaces == NULL)
+		return 1;
+	else
+	{
+		net_iface* cursor = interfaces;
+		unsigned short found = 0;
+
+		while(found == 0 && cursor != NULL)
+		{
+			if(strcmp(cursor->name,name) == 0)
+			{
+				found = 1;
+				cursor->ip = ip;
+			}
+			else
+				cursor = cursor->next;
+		}
+
+		if(found == 0)
+			return 1;
+	}
+
+	return 0;
+}
+
+char* getInterfaceIP(char* name)
+{
+	char _ip[50];
+
+	if(interfaces == NULL)
+		return "";
+	else
+	{
+		net_iface* cursor = interfaces;
+		unsigned short found = 0;
+
+		while(found == 0 && cursor != NULL)
+		{
+			if(strcmp(cursor->name,name) == 0)
+			{
+				found = 1;
+				strcpy(_ip,cursor->ip);
+			}
+			else
+				cursor = cursor->next;
+		}
+
+		if(found == 0)
+			return "";
+	}
+
+	return _ip;
+}
+
+unsigned short setInterfaceDesc(char* name, char* desc)
+{
+	if(interfaces == NULL)
+		return 1;
+	else
+	{
+		net_iface* cursor = interfaces;
+		unsigned short found = 0;
+
+		while(found == 0 && cursor != NULL)
+		{
+			if(strcmp(cursor->name,name) == 0)
+			{
+				found = 1;
+				cursor->desc = desc;
+			}
+			else
+				cursor = cursor->next;
+		}
+
+		if(found == 0)
+			return 1;
+	}
+
+	return 0;
+}
+
+char* getInterfaceDesc(char* name)
+{
+	char _desc[50];
+
+	if(interfaces == NULL)
+		return "";
+	else
+	{
+		net_iface* cursor = interfaces;
+		unsigned short found = 0;
+
+		while(found == 0 && cursor != NULL)
+		{
+			if(strcmp(cursor->name,name) == 0)
+			{
+				found = 1;
+				strcpy(_desc,cursor->desc);
+			}
+			else
+				cursor = cursor->next;
+		}
+
+		if(found == 0)
+			return "";
+	}
+
+	return _desc;
+}
+
+unsigned short setInterfaceState(char* name, unsigned short state)
+{
+	if(interfaces == NULL)
+		return 1;
+	else
+	{
+		net_iface* cursor = interfaces;
+		unsigned short found = 0;
+
+		while(found == 0 && cursor != NULL)
+		{
+			if(strcmp(cursor->name,name) == 0)
+			{
+				found = 1;
+				cursor->state = state;
+			}
+			else
+				cursor = cursor->next;
+		}
+
+		if(found == 0)
+			return 1;
+	}
+
+	return 0;
+}
+
+unsigned short getInterfaceState(char* name)
+{
+	unsigned short result = -1;
+
+	if(interfaces == NULL)
+		return -1;
+	else
+	{
+		net_iface* cursor = interfaces;
+		unsigned short found = 0;
+
+		while(found == 0 && cursor != NULL)
+		{
+			if(strcmp(cursor->name,name) == 0)
+			{
+				found = 1;
+				result = cursor->state;
+			}
+			else
+				cursor = cursor->next;
+		}
+
+		if(found == 0)
+			return -1;
+	}
+
+	return result;
+}
+
+unsigned short setInterfaceACL(char* name, char* aclname, char* direction)
+{
+	if(interfaces == NULL)
+		return 1;
+	else
+	{
+		net_iface* cursor = interfaces;
+		unsigned short found = 0;
+
+		while(found == 0 && cursor != NULL)
+		{
+			if(strcmp(cursor->name,name) == 0)
+			{
+				found = 1;
+
+				acl* cursor2 = access_lists;
+				unsigned short found2 = 0;
+
+				while(found2 == 0 && cursor2 != NULL)
+				{
+					if(strcmp(cursor2->name,aclname) == 0)
+					{
+						found2 = 1;
+
+						if(strcmp(direction,"in") == 0)
+							cursor->acl_in = aclname;
+						else if(strcmp(direction,"out") == 0)
+							cursor->acl_out = aclname;
+						else
+							return 1;
+					}
+					else
+						cursor2 = cursor2->next;
+				}
+
+				if(found2 == 0)
+					return 1;
+			}
+			else
+				cursor = cursor->next;
+		}
+
+		if(found == 0)
+			return 1;
+	}
+
+	return 0;
+}
